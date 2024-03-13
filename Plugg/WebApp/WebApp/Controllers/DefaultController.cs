60a1fbfaef5286e5d5dbe524b4f2aadcd274e934
HugoEddesten <hugo.eddesten@yh.nackademin.se>
2024-03-13 12:34:21 +0100

    Init

---
+++ Plugg/WebApp/WebApp/Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class DefaultController : Controller
+    {
+        [Route("/")]
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        [Route("/contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
